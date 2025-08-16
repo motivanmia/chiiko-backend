0e00b750efe7ef8bc9a78eb8660e7589d5e97f99
Grey-Liu <sugarfree5807@gmail.com>
2025-08-16 11:04:42 +0800

    fix(school):修改get

--- front/school/getIngredients.php
+++ front/school/getIngredients.php
@@ -1,34 +1,29 @@
 <?php
-  header('Access-Control-Allow-Origin: http://localhost:5173');
-
-  require_once __DIR__ . '/common/config.php';
+  require_once __DIR__ . '/../../common/conn.php';
+  require_once __DIR__ . '/../../common/cors.php';
+  require_once __DIR__ . '/../../common/functions.php';
   
-  $conn = new mysqli(
-      DB_HOST,
-      DB_USER,
-      DB_PSW,
-      DB_NAME,
-      DB_PORT
-  );
-  if ($conn->connect_error) {
-      die(json_encode(["error" => "連線失敗: " . $conn->connect_error]));
-  }
-
-  header('Content-Type: application/json; charset=utf-8');
+  require_method('GET');
 
   $sql = "SELECT * FROM ingredients";
-  $result = $conn->query($sql);
 
-  $data = [];
-  while ($row = $result->fetch_assoc()) {
-    // 若欄位是 JSON/TEXT 都可以這樣處理
-    $row['image']   = $row['image']   ? json_decode($row['image'], true) : [];
-    $row['content'] = $row['content'] ? json_decode($row['content'], true) : [];
-    $row['status']  = (int)($row['status'] ?? 0);
-    $row['ingredients_categary_id'] = (int)($row['ingredients_categary_id'] ?? 0);
-    $data[] = $row;
-  }
-  echo json_encode($data, JSON_UNESCAPED_UNICODE);
+  // 取得資料
+  $result = db_query($mysqli, $sql);
+  $data = $result->fetch_all(MYSQLI_ASSOC);
 
+  // $data = [];
+  // while ($row = $result->fetch_assoc()) {
+  //   $row['image']   = $row['image']   ? json_decode($row['image'], true) : [];
+  //   $row['content'] = $row['content'] ? json_decode($row['content'], true) : [];
+  //   $row['status']  = (int)($row['status'] ?? 0);
+  //   $row['ingredients_categary_id'] = (int)($row['ingredients_categary_id'] ?? 0);
+  //   $data[] = $row;
+  // }
+
+  send_json([
+    'status' => 'success',
+    'message' => '資料取得成功',
+    'data' => $data
+  ]);
   $conn->close();
 ?>
