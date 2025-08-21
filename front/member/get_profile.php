ad0d6ab92153f331b67cf9b61ee872de391b1d98
motivanmia <waniowo@gmail.com>
2025-08-21 12:45:12 +0800

    fix(member):修改get會員資料

--- front/member/get_profile.php
+++ front/member/get_profile.php
@@ -6,8 +6,19 @@
   // 只允許使用 GET
   require_method('GET');
 
-  // 取得哪個使用者
-  $user_id = get_int_param('user_id');
+  // 檢查session中是否有使用者id 如果沒有表示未登入
+  if(!isset($_SESSION['user_id'])){
+    // 傳送未授權回應 並終止程式
+    http_response_code(401);
+    send_json([
+      'status' => 'error',
+      'message' => '未授權，請先登入'
+    ]);
+    exit;
+  }
+
+  // 從session中取得當前登入的使用者id
+  $current_user_id = $_SESSION['user_id'];
 
   // SQL 查詢
   $sql = "SELECT 
@@ -18,16 +29,33 @@
       account,
       address,
       image
-      FROM users";
+    FROM users
+    WHERE user_id = ?";
 
-  // 取得資料
-  $result = db_query($mysqli, $sql);
-  $data = $result->fetch_all(MYSQLI_ASSOC);
+  $stmt = $mysqli -> prepare($sql);
+  $stmt -> bind_param('i', $current_user_id);
+  $stmt -> execute();
+  $result = $stmt -> get_result();
 
-  // 成功回應
-  send_json([
-    'status' => 'success',
-    'message' => '資料取得成功',
-    'data' => $data
-  ]);
+  if ($result -> num_rows > 0){
+    // 成功取得資料，只會有一筆
+    $data = $result -> fetch_assoc();
+    // 成功回應
+    send_json([
+      'status' => 'success',
+      'message' => '資料取得成功',
+      'data' => $data
+    ]);
+  }else{
+    // 找不到使用者資料
+    send_json([
+      'status' => 'error',
+      'message' => '找不到使用者資料'
+    ]);
+  }
+
+  // 關閉語法與連線
+  $stmt->close();
+  $mysqli->close();
+
 ?>
